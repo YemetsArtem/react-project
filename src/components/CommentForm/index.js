705fbaa5bd3afb915c0758c570ac3c52637896db
YemetsArtem <artem.yemets@asimovcode.com>
2019-09-23 18:19:20 +0300

    add comment-form

--- src/components/CommentForm/index.js
+++ src/components/CommentForm/index.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addComment } from '../../ac';
 import "./style.css";
 
 class CommentForm extends Component {
+  state = {
+      id:"123",
+      name:"User",
+      text:"Default"
+  }
+
   render() {
     return (
-        <form className="new_message">
-            <input className="user_name" placeholder="Enter user name" />
-            <input className="text" placeholder="Enter text" />
-            <button>Save</button>
-        </form>
+        <div className="new_message">
+            <input onChange={this.handleChange("name")} className="user_name" placeholder="Enter user name" />
+            <input onChange={this.handleChange("text")}  className="text" placeholder="Enter text" />
+            <button onClick={this.handleClick}>Save</button>
+        </div>
     );
+  }
+
+  handleChange = (type) => (e) => {
+    this.setState({
+      [type]: e.target.value
+    })
+  }
+
+  handleClick = () => {
+    console.log(this.state);
+    addComment(this.state);
   }
 }
 
-export default CommentForm;
+export default connect(null, addComment)(CommentForm);
